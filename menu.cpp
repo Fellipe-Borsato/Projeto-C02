e10ab475d4ff9496022dfe142deded27fcf47099
Fellipe Borsato <fellipeborsato@gmail.com>
2025-11-12 22:12:29 -0300

    improve menu system

--- menu.cpp
+++ menu.cpp
@@ -1,26 +1,15 @@
 #include <iostream>
+#include "struct.cpp"
 
 using namespace std;
 
-int menu()
+carro menu(int o)
 {
-    int opcao, condicao = 0;
-    int escolhas[4];
+    int condicao = 0;
+    short escolha;
+    carro Carro;
 
-    while(condicao==0)
-    {
-        cout << "|    MENU     |" << endl;
-        cout << "Escolha uma opcao:" << endl;
-        cout << "1. Cor" << endl;
-        cout << "2. Interior" << endl;
-        cout << "3. Rodas" << endl;
-        cout << "4. Carroceria" << endl;
-        cout << "5. Motor" << endl;
-        cout << "6. Finalizar" << endl;
-
-        cin >> opcao;
-
-        switch opcao:
+        switch(o)
         {
             case 1:
                 cout << "Escolha a cor desejada:" << endl;
@@ -28,45 +17,87 @@ int menu()
                 cout << "2. Vermelho" << endl;
                 cout << "3. Preto" << endl;
                 cout << "4. Cinza" << endl;
-                cin >> escolhas[0];
+                cout << " " << endl;
+                cin >> escolha;
+
+                switch (escolha){
+                    case 1: Carro.cor = "Branco"; break;
+                    case 2: Carro.cor = "Vermelho"; break;
+                    case 3: Carro.cor = "Preto"; break;
+                    case 4: Carro.cor = "Cinza"; break;
+                }
                 break;
+
             case 2:
                 cout << "Escolha o interior desejado:" << endl;
                 cout << "1. Esportivo" << endl;
                 cout << "2. Luxo" << endl;
                 cout << "2. Simples" << endl;
-                cin >> escolhas[1];
+                cout << " " << endl;
+
+                cin >> escolha;
+
+                switch (escolha){
+                    case 1: Carro.interior = "Esportivo"; break;
+                    case 2: Carro.interior = "Luxo"; break;
+                    case 3: Carro.interior = "Simples"; break;
+                }
                 break;
+
             case 3:
                 cout << "Escolha as rodas desejadas:" << endl;
                 cout << "1. Pista" << endl;
                 cout << "2. Drift" << endl;
-                cin >> escolhas[2];
+                cout << " " << endl;
+
+                cin >> escolha;
+
+                switch (escolha){
+                    case 1: Carro.rodas = "Pista"; break;
+                    case 2: Carro.rodas = "Drift"; break;
+                }
                 break;
+
             case 4:
                 cout << "Escolha a carroceria desejada:" << endl;
                 cout << "1. Conversivel" << endl;
                 cout << "2. Sedan" << endl;
                 cout << "3. SUV" << endl;
                 cout << "4. Simples" << endl;
-                cin >> escolhas[3];
+                cout << " " << endl;
+                cin >> escolha;
+
+                switch (escolha){
+                    case 1: Carro.carroceria = "Branco"; break;
+                    case 2: Carro.carroceria = "Vermelho"; break;
+                    case 3: Carro.carroceria = "Preto"; break;
+                    case 4: Carro.carroceria = "Cinza"; break;
+                }
                 break;
+
             case 5:
                 cout << "Escolha o motor desejado:" << endl;
                 cout << "1. V10" << endl;
                 cout << "2. V6" << endl;
                 cout << "3. V8" << endl;
-                cin >> escolhas[4];
+                cout << " " << endl;
+                cin >> escolha;
+
+                switch (escolha){
+                    case 1: Carro.motor = "V10"; break;
+                    case 2: Carro.motor = "V6"; break;
+                    case 3: Carro.motor = "V8"; break;
+                }
                 break;
+
             case 6:
                 cout << "Finalizando escolha..." << endl;
                 condicao = 1;
                 break;
+
             default:
                 cout << "Opcao invalida. Tente novamente." << endl;
         }
 
-    }
-
-    return escolhas;
+    return Carro;
 }
---
+++ struct.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+struct carro{
+    string carroceria, interior, cor, rodas, motor;
+
+    string GetCor(){return cor;};
+};
+
+class moto{
+    string carroceria, interior, cor, rodas, motor;
+};
+
